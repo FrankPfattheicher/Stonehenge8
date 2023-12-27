dd586cc98eb11a391873b030a6d24fdd2af9c7d7
Frank Pfattheicher <fpf@ict-baden.de>
2023-12-27 16:16:35 +0100

    Refactoring

--- IctBaden.Stonehenge.Vue.SampleCore/ViewModels/MermaidViewVm.cs
+++ IctBaden.Stonehenge.Vue.SampleCore/ViewModels/MermaidViewVm.cs
@@ -5,6 +5,7 @@ using IctBaden.Stonehenge.ViewModel;
 
 namespace IctBaden.Stonehenge.Vue.SampleCore.ViewModels;
 
+// ReSharper disable once UnusedType.Global
 public class MermaidViewVm : ActiveViewModel
 {
     public string MermaidGraph { get; private set; } = string.Empty;
@@ -22,25 +23,29 @@ public class MermaidViewVm : ActiveViewModel
         switch (graph)
         {
             case 1:
-                MermaidGraph = @"graph TD;  Start --> Stop;";
-                MermaidGraph2 = @"graph TD;  Begin --> End;";
+                MermaidGraph = "graph TD;  Start --> Stop;";
+                MermaidGraph2 = "graph TD;  Begin --> End;";
                 break;
             case 2:
-                MermaidGraph = @"graph LR;
-    A--> B & C & D;
-    B--> A & E;
-    C--> A & E;
-    D--> A & E;
-    E--> B & C & D;";
-                MermaidGraph2 = @"sequenceDiagram
-    Alice ->> Bob: Hello Bob, how are you?
-    Bob-->>John: How about you John?
-    Bob--x Alice: I am good thanks!
-    Bob-x John: I am good thanks!
-    Note right of John: Bob thinks a long<br/>long time, so long<br/>that the text does<br/>not fit on a row.
-
-    Bob-->Alice: Checking with John...
-    Alice->John: Yes... John, how are you?";
+                MermaidGraph = """
+                               graph LR;
+                                   A--> B & C & D;
+                                   B--> A & E;
+                                   C--> A & E;
+                                   D--> A & E;
+                                   E--> B & C & D;
+                               """;
+                MermaidGraph2 = """
+                                sequenceDiagram
+                                    Alice ->> Bob: Hello Bob, how are you?
+                                    Bob-->>John: How about you John?
+                                    Bob--x Alice: I am good thanks!
+                                    Bob-x John: I am good thanks!
+                                    Note right of John: Bob thinks a long<br/>long time, so long<br/>that the text does<br/>not fit on a row.
+                                
+                                    Bob-->Alice: Checking with John...
+                                    Alice->John: Yes... John, how are you?
+                                """;
                 break;
         }
     }
